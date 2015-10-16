9d16616aa311f7d57898d344ab3cda5aac1e58b1
Lyly Nguyen <lylynguyen812@gmail.com>
2015-10-15 18:04:42 -0700

    Pass getElementsByClassNameSpec

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  var match = [];
+
+  searchList(document.body);
+
+  function searchList(element) {
+    filterName(element);
+
+    var children = element.childNodes;
+    for(var child in children) {
+      searchList(children[child]);
+    }
+  };
+
+  function filterName(element) {
+    var elementClass = element.classList;
+    if(elementClass) {
+      for(var name = 0; name < elementClass.length; name++) {
+        if(elementClass[name] === className) {
+          match.push(element);
+        }
+      }
+    }
+  }
+  return match;
 };
