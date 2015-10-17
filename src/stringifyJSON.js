5c81bb1d5015c514b63a72b79eba6b8083b4c499
Lyly Nguyen <lylynguyen812@gmail.com>
2015-10-17 00:45:29 -0700

    Pass stringifyJSONSpec

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,46 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+var result = [];
+  //check for arrays
+  if(Array.isArray(obj)){
+    for(var i=0;i<obj.length;i++){
+      if(obj[i]===undefined && typeof obj[i] === 'function'){
+        result.push(stringifyJSON(null));
+      } else {
+        result.push(stringifyJSON(obj[i]));
+      }
+    }
+    return '[' + result.join(',') + ']';
+  }
+  //check for objects
+  if(obj && typeof obj === 'object'){
+    for(var key in obj){
+      if(obj[key]!==undefined && typeof obj[key] !== 'function'){
+        result.push(stringifyJSON(key)+":"+stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + result.join(',') + '}';
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } 
+//stringify primitives
+ return  '' + obj + '';
 };
+
+
+/*
+- Number 
+- String 
+- Boolean 
+- Array 
+- Object 
+- Function 
+- Regular Expression 
+- Null 
+- Undefined 
+*/
